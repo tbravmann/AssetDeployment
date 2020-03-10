320659c7f176f363de0792398138fd6d7f9350a8
Tobi <tbravmann@bavariantools.org>
2020-03-10 20:10:18 +0100

    AssetDeployment Working

---
+++ src/webparts/spfxAssets/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface ISpfxAssetsWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'SpfxAssetsWebPartStrings' {
+  const strings: ISpfxAssetsWebPartStrings;
+  export = strings;
+}
